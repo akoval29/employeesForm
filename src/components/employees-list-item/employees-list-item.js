a89d739cbc28fc6e84c0783c32cc37ce7787528c
akoval29 <akoval29@gmail.com>
2022-05-08 23:27:09 +0300

    style_inline

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -14,8 +14,8 @@ const EmployeesListItem = (props) => {
                 <span type = "button" 
                     className="list-group-item-label"
                     onClick = {onToggleProp} 
-                    data-toggle="rise">
-                    {name}</span>
+                    data-toggle="rise"
+                    style={{fontSize: '20', color: 'gray', transition: 'all', WebkitTransition: 'all', msTransition: 'all'}}>{name}</span> 
                 <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
