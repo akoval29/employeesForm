7d8d1eb1fd7bbab993cff9c18ba0415cded66f8b
akoval29 <akoval29@gmail.com>
2022-05-25 01:26:43 +0300

    cleaned up

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,123 +7,6 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-// Передача аргументів - Варіант № 1
-function WhoAmI1 (props) {
-  return (
-    <div>
-      <h1>My name is {props.name}, lastname - {props.lastname}</h1>
-      <a href={props.link}>My profile</a>
-    </div>
-  )
-}
-
-// Передача аргументів - Варіант № 2 - деструктуризація
-function WhoAmI2 ({name, lastname, link}) {
-  return (
-    <div>
-      <h1>My name is {name}, lastname - {lastname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
-}
-
-// Передача аргументів - Варіант № 3 - передали СВОЙСТВО ОБЄКТА
-function WhoAmI3 ({name, lastname, link}) {
-  return (
-    <div>
-      <h1>My name is {name.firstName}, lastname - {lastname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
-}
-
-// Передача аргументів - Варіант № 4 - передали ФУНКЦІЮ
-function WhoAmI4 ({name, lastname, link}) {
-  return (
-    <div>
-      <h1>My name is {name()}, lastname - {lastname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
-}
-
-// Класи
-class WhoAmI5 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      years: 27,
-      textPlus: "Plus 1",
-      textMinus: "Minus 1",
-      pos: "",
-    }
-    this.nextYear2 = this.nextYear2.bind(this);  //второй метод привязки контекста
-  }
-
-// setState - запускает перерисовку компонента
-  nextYear = () => {
-    this.setState({
-      years: this.state.years +1
-    })
-  }
-
-  // setState - запускает перерисовку компонента - второй и третий метод превязки контекста (разница в рендер-ретурне)
-  nextYear2() {
-    this.setState(state => ({
-      years: this.state.years +1
-    }))
-  }
-
-// а такой вариант для случаев когда надо подождать асинхрон от "какой-то" предыдущей операции над ЭТИМ стейтом
-  prefYear = () => {
-    this.setState(state => ({
-      years: state.years -1
-    }))
-  }
-
-  //событие инпута. передали в сетстейт обьект, изменили стейт.
-  commitInputOnChanges = (e, color) => {
-    console.log(color);
-    console.log(e.target);
-    console.log(e.target.value);
-    this.setState({
-      pos: e.target.value
-    })
-  }
-
-  render() {
-    const {name, lastname, link} = this.props;
-    const {years, pos}= this.state;
-    return (
-      <div>
-        {/* Первый метод привязки контекста*/}
-        <button onClick={this.nextYear} >{this.state.textPlus}</button>
-        <button onClick={this.prefYear} >{this.state.textMinus}</button>
-        
-        {/* Второй метод */}
-        <button onClick={this.nextYear2} >{this.state.textPlus}</button>
-        
-        {/* Третий метод */}
-        <button onClick={() => this.nextYear2()} >{this.state.textPlus}</button>
-
-
-        <h1>My name is {name}, 
-        lastname - {lastname}, 
-        age - {years}, 
-        input - {pos}</h1>
-
-        <a href={link}>My profile</a>
-        
-        <form>
-          <span>Введите должность</span>
-          <input type="text" onChange={(e) => this.commitInputOnChanges(e, 'some color')}/> 
-        </form>
-        {/* еще варианты = onChange - onClick - onSubmit - onInput */}
-        {/* аргументы можна и не передавать, тогда будет:   <input type="text" onChange={this.commitInputOnChange}/>   */}
-      </div>
-    )
-  }
-}
 
 class App extends Component {
   constructor(props) {
@@ -153,7 +36,7 @@ class App extends Component {
       //   data: newArr
       // }
 
-      // удобний метод
+      // удобний метод =)
       return {
         data: data.filter(item => item.id !== id)
       }
@@ -294,12 +177,6 @@ class App extends Component {
         
         <EmployeesAddForm 
           onAdd={this.addItem}/>
-        
-        <WhoAmI1 name="John" lastname="Smith" link="facebook.com"/>
-        <WhoAmI2 name="Vin" lastname="Diesel" link="facebook.com"/>
-        <WhoAmI3 name={{firstName: "Part of"}} lastname="Object" link="facebook.com"/>
-        <WhoAmI4 name={() => {return "Function here !!!!!"}} lastname="realy!" link="facebook.com"/>
-        <WhoAmI5 name="we use classes!!!" lastname="here!!" link="facebook.com"/>
       </div>
     );
   }
